8a2a938ca46a8435e2dee34c291e4d5d1c104be4
Aditya Purkar <aspurkar@gmail.com>
2017-05-13 12:25:32 -0700

    final commit

---
+++ PartialViewChildActions/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using PartialViewChildActions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialViewChildActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [ChildActionOnly]
+        public ActionResult show()
+        {
+            var stud = new Student();
+            stud.Name = "Aditya";
+            stud.Email = "adi@yahoo.com";
+            stud.Mpbile = "76574564654";
+            stud.StudentId = 1001;
+            stud.Address = new Address();
+            stud.Address.City = "San Fran";
+            stud.Address.Country = "United States";
+            stud.Address.State = "California";
+            return View(stud);
+        }
+        public ActionResult show2()
+        {
+            return View();
+        }
+    }
+}
---
+++ PartialViewChildActions/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartialViewChildActions.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mpbile { get; set; }
+        public Address Address { get; set; }
+    }
+    public class Address
+    {
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Pincode { get; set; }
+        public string Street { get; set; }
+    }
+}
